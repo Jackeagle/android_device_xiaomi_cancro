2b492c33a6bce1638c08932ac100d0c19cf04153
audahadi <wan.audahadi@gmail.com>
2014-10-12 22:41:05 +0800

    initial aicp bringup

--- full_cancro.mk
+++ full_cancro.mk
@@ -22,22 +22,14 @@
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/xiaomi/cancro/cancro.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/xiaomi/cancro/cancro.mk)
-
-# Build fingerprint
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Xiaomi/cancro/cancro:4.4.2/KVT49L/KXDMIBE16.0:user/release-keys PRIVATE_BUILD_DESC="cancro-user 4.4.2 KVT49L KXDMIBE16.0 release-keys"
-
 # Discard inherited values and use our own instead.
+PRODUCT_NAME := full_cancro
 PRODUCT_DEVICE := cancro
-PRODUCT_NAME := pa_cancro
-PRODUCT_BRAND := xiaomi
-PRODUCT_MODEL := MI 3W
-PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Full AOSP on Cancro
